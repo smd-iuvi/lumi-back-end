6306461da783c591c187a76afe3740a63bbd2ab1
Paulo José <paulocardosob@gmail.com>
2020-03-24 18:08:30 -0300

    Update GenreRoutes

--- src/Controllers/GenreController.ts
+++ src/Controllers/GenreController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 
 import Genre from '../Schemas/Genre'
+import Video from '../Schemas/Video'
 
 class GenreController {
   public async index (req: Request, res: Response): Promise<Response> {
@@ -16,6 +17,33 @@ class GenreController {
     try {
       const genre = await Genre.create(req.body)
       return res.json(genre)
+    } catch (error) {
+      return res.json(error)
+    }
+  }
+
+  public async update (req: Request, res: Response): Promise<Response> {
+    try {
+      const genre = await Genre.findOneAndUpdate({ _id: req.params.id }, req.body)
+      return res.json(genre)
+    } catch (error) {
+      return res.json(error)
+    }
+  }
+
+  public async delete (req: Request, res: Response): Promise<Response> {
+    try {
+      const genre = await Genre.findByIdAndDelete({ _id: req.params.id })
+      return res.json(genre)
+    } catch (error) {
+      return res.json(error)
+    }
+  }
+
+  public async getVideos (req: Request, res: Response): Promise<Response> {
+    try {
+      const videos = await Video.find({ genre: req.params.id })
+      return res.json(videos)
     } catch (error) {
       return res.json(error)
     }
