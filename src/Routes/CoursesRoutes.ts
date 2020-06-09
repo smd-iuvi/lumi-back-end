7daa900a2ccff979061276c22a3926b1f226a9e7
Paulo José <paulocardosob@gmail.com>
2020-06-09 18:10:42 -0300

    Implement get videos of course

--- src/Routes/CoursesRoutes.ts
+++ src/Routes/CoursesRoutes.ts
@@ -9,6 +9,8 @@ routes.post('/courses', CourseController.create)
 routes.put('/courses', CourseController.update)
 routes.delete('/courses', CourseController.delete)
 
+routes.get('/courses/:id', CourseController.getCourse)
+
 routes.get('/courses/:id/videos', CourseController.getVideos)
 routes.get('/courses/:id/events', CourseController.getEvents)
 
