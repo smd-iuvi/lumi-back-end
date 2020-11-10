13035e34a8ee591c54313f5e14da424aea11ba42
Paulo Barbosa <paulo.barbosa@hp.com>
2020-11-10 12:05:02 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,38 +1,21 @@
 import { Router } from 'express'
-
-import UserController from './Controllers/UserController'
-import StudentController from './Controllers/StudentController'
-import GenreController from './Controllers/GenreController'
-
-import VideoRoutes from './Routes/VideosRoutes'
+import AuthRoutes from './Routes/AuthRoutes'
+import CommentRoutes from './Routes/CommentsRoutes'
 import CourseRoutes from './Routes/CoursesRoutes'
 import EventsRoutes from './Routes/EventsRoutes'
-
-import TeacherRoutes from './Routes/TeacherRoutes'
-import StudentRoutes from './Routes/StudentsRoutes'
-
+import GenreRoutes from './Routes/GenreRoutes'
 import SemesterRoutes from './Routes/SemesterRoutes'
-
-import AuthRoutes from './Routes/AuthRoutes'
-
-import CommentRoutes from './Routes/CommentsRoutes'
-
 import TagRoutes from './Routes/TagRoutes'
+import UserRoutes from './Routes/UserRoutes'
+import VideoRoutes from './Routes/VideosRoutes'
 
 const routes = Router()
 
-routes.get('/users', UserController.index)
-
-routes.get('/students', StudentController.index)
-
-routes.get('/genres', GenreController.index)
-routes.post('/genres', GenreController.create)
-
+routes.use(UserRoutes)
+routes.use(GenreRoutes)
 routes.use(VideoRoutes)
 routes.use(CourseRoutes)
 routes.use(EventsRoutes)
-routes.use(TeacherRoutes)
-routes.use(StudentRoutes)
 routes.use(SemesterRoutes)
 routes.use(CommentRoutes)
 routes.use(TagRoutes)
